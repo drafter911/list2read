11f4f06c18d555653eee923a630cf28f8cf419e9
Vladimir Panchuk <drafter911@gmail.com>
2016-10-03 13:38:49 +0300

    [server] added basic articles routing

--- server/app.js
+++ server/app.js
@@ -23,6 +23,8 @@ app.get('/', (req, res) => {
     //})
 });
 
+
+//BOOKS
 app.get('/books', (req, res) => {
     db.collection('books').find().toArray((err, result) => {
         if (err) {
@@ -41,7 +43,7 @@ app.post('/books', (req, res) => {
             return console.log(err);
         }
 
-        console.log('saved to database');
+        console.log('book saved to database');
         res.redirect('/');
     })
 });
@@ -68,9 +70,64 @@ app.delete('/books', (req, res) => {
     db.collection('books').findOneAndDelete({title: req.body.title},
         (err, result) => {
             if (err) return res.send(500, err);
-            res.send('Selected item was successfully deleted.')
+            res.send('Selected book was successfully deleted.')
         })
 });
+//END_BOOKS
+
+//ARTICLES
+app.get('/articles', (req, res) => {
+    db.collection('articles').find().toArray((err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+        res.send(result)
+    })
+});
+
+app.post('/articles', (req, res) => {
+    db.collection('articles').save(req.body, (err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('article saved to database');
+        res.redirect('/');
+    })
+});
+
+app.put('/articles', (req, res) => {
+    db.collection('articles')
+        .findOneAndUpdate({title: req.body.itemToUpdate}, {
+            $set: {
+                author: req.body.author,
+                title: req.body.title
+            }
+        }, {
+            sort: {_id: -1},
+            upsert: true
+        }, (err, result) => {
+            if (err) {
+                return res.send(err)
+            }
+            res.send(result);
+        })
+});
+
+app.delete('/articles', (req, res) => {
+    db.collection('articles').findOneAndDelete({title: req.body.title},
+        (err, result) => {
+            if (err) return res.send(500, err);
+            res.send('Selected article was successfully deleted.')
+        })
+});
+//END_ARTICLES
+
+
+
+
+
+
 
 const MongoClient = require('mongodb').MongoClient;
 
